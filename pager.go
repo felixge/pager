36f834646e8e0b0b61392a5a871e104c0d0849a5
Felix Geisendörfer <felix@debuggable.com>
2014-07-09 15:50:51 +0200

    WIP

--- pager.go
+++ pager.go
@@ -4,11 +4,17 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
 func Start(name string, arg ...string) (*Pager, error) {
-	pager := &Pager{cmd: exec.Command(name, arg...)}
+	lock := &sync.Mutex{}
+	pager := &Pager{
+		cmd:  exec.Command(name, arg...),
+		lock: lock,
+		cond: sync.NewCond(lock),
+	}
 	if err := pager.start(); err != nil {
 		return nil, err
 	}
@@ -16,10 +22,13 @@ func Start(name string, arg ...string) (*Pager, error) {
 }
 
 type Pager struct {
+	lock       *sync.Mutex
 	cmd        *exec.Cmd
-	pipeStdout *os.File
-	realStdout *os.File
-	waiting    bool
+	pipeR      *os.File
+	realStdout int
+	cond       *sync.Cond
+	wait       *error
+	pipeClose  *error
 }
 
 func (p *Pager) start() error {
@@ -32,40 +41,83 @@ func (p *Pager) start() error {
 	if err := p.cmd.Start(); err != nil {
 		return err
 	}
+	// We cannot close the read end at this point. Otherwise we may get a SIGPIPE
+	// if the pager terminates quickly after starting. So we keep a reference for
+	// closing this later when we're sure the write end isn't referenced anymore.
+	p.pipeR = r
 	stdoutFd, err := syscall.Dup(int(os.Stdout.Fd()))
 	if err != nil {
 		return err
 	}
-	p.realStdout = os.NewFile(uintptr(stdoutFd), "stdout backup")
+	p.realStdout = stdoutFd
 	if err := syscall.Dup2(int(w.Fd()), int(os.Stdout.Fd())); err != nil {
 		return err
 	}
-	p.pipeStdout = os.Stdout
 	if err := w.Close(); err != nil {
 		return err
 	}
+	exitCh := make(chan error)
+	go func() {
+		exitCh <- p.cmd.Wait()
+	}()
 	sigCh := make(chan os.Signal)
 	signal.Notify(sigCh, os.Interrupt)
 	go func() {
+		defer signal.Stop(sigCh)
 		for {
-			sig := <-sigCh
-			p.cmd.Process.Signal(sig)
-			p.pipeStdout.Close()
+			select {
+			case sig := <-sigCh:
+				// Forward the signal to our pager, so he gets to decide what to do
+				// with this.
+				p.cmd.Process.Signal(sig)
+				// Break the pipe to indicate that we are done sending data to the
+				// pager. This allows users to jump to the bottom using F in less
+				// and mirrors the way git log works.
+				p.closePipe()
+			case err := <-exitCh:
+				closeErr := p.closePipe()
+				p.cond.L.Lock()
+				if closeErr != nil {
+					p.wait = &closeErr
+				} else {
+					p.wait = &err
+				}
+				p.cond.L.Unlock()
+				p.cond.Broadcast()
+				return
+			}
 		}
 	}()
 	return nil
 }
 
+// closePipe
+func (p *Pager) closePipe() error {
+	p.cond.L.Lock()
+	defer p.cond.L.Unlock()
+	if p.pipeClose != nil {
+		return *p.pipeClose
+	}
+	devNull, err := os.Open(os.DevNull)
+	p.pipeClose = &err
+	if err != nil {
+		return err
+	}
+	return syscall.Dup2(int(devNull.Fd()), int(os.Stdout.Fd()))
+}
+
+// Wait waits for the pager to exit. Can be called my multiple goroutines.
 func (p *Pager) Wait() error {
-	p.pipeStdout.Close()
-	os.Stdout = p.realStdout
-	return p.cmd.Wait()
-	//defer func() {
-	//os.Stdout = p.stdout
-	//}()
-	//os.Stdout.Close()
-	//exitCh := make(chan error)
-	//go func() {
-	//exitCh <- p.cmd.Wait()
-	//}()
+	if err := p.closePipe(); err != nil {
+		return err
+	}
+	p.cond.L.Lock()
+	defer p.cond.L.Unlock()
+	for p.wait == nil {
+		p.cond.Wait()
+	}
+	if err := syscall.Dup2(p.realStdout, int(os.Stdout.Fd())); err != nil {
+		return err
+	}
+	return *p.wait
 }
